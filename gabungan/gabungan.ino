f54847c2953a7604299729c277b3ab995e648a3c
Muhammad Dzakwan Attaqiy <98676984+mhddzakwan@users.noreply.github.com>
2024-09-28 20:46:31 +0700

    Update gabungan.ino

--- gabungan/gabungan.ino
+++ gabungan/gabungan.ino
@@ -2,15 +2,28 @@
 
 Servo myservo;
 
-#define servopin 9
+#define servopin 9  
 #define trigpin 2
 #define echopin 3
+
+#DEFINE LED_RED 7
+#DEFINE LED_YELLOW 8
+#DEFINE LED_GREEN 10
 
 long durasi;
 int jarak;
 int pos = 0;
 
 void setup() {
+  //inisiasi LED
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_YELLOW, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
+
+  digitalWrite(LED_RED, LOW);
+  digitalWrite(LED_YELLOW, HIGH);
+  digitalWrite(LED_GREEN, LOW);
+  
   //inisialisasi servo pin
   myservo.attach(servopin);
 
@@ -43,8 +56,31 @@ void loop() {
   int trigger_pos = 20;
 
   if (jarak < trigger_pos){
-    pos = 90;
-    myservo.write(pos);
+    digitalWrite(LED_RED, HIGH);
+    digitalWrite(LED_YELLOW, LOW);
+    digitalWrite(LED_GREENN, LOW);
+
+    //Membuka tong sampah
+    for(pos=0; pos <=90; pos++){
+      myservo.write(pos);
+      delay(20);
+    }
+    digitalWrite(LED_RED, LOW);
+    digitalWrite(LED_YELLOW, LOW);
+    digitalWrite(LED_GREENN, HIGH);
+    delay(2000);
+    digitalWrite(LED_RED, HIGH);
+    digitalWrite(LED_YELLOW, LOW);
+    digitalWrite(LED_GREENN, LOW);
+    //Menutup tong sampah
+    for(pos=90; pos >=0; pos--){
+      myservo.write(pos);
+      delay(20);
+    }
+
+    digitalWrite(LED_RED, LOW);
+    digitalWrite(LED_YELLOW, HIGH);
+    digitalWrite(LED_GREENN, LOW);
   } else{
     pos = 0;
     myservo.write(pos);
